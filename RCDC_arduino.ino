097b147f2259ede83aacc7512d681d29da390bd0
Miłosz Tęsiorowski <168329245+miloszek420@users.noreply.github.com>
2025-04-18 00:21:40 +0200

    basic wersja (20pkt)

---
+++ RCDC_arduino.ino
@@ -0,0 +1,28 @@
+
+const int BUTTON_PIN = 2
+const int LED_PIN_1 = 3
+const int LED_PIN_2 = 5
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState==1)
+    {
+       analogWrite(LED_PIN_1,127);
+       analogWrite(LED_PIN_2,127);
+       delay(10000);
+       analogWrite(LED_PIN_1,0);
+       delay(20000)
+       analogWrite(LED_PIN_2,0);
+    }
+    
+}
