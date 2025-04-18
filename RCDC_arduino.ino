9617d300de86cc9e5b4e36830734b4726598ad3a
Miłosz Tęsiorowski <168329245+miloszek420@users.noreply.github.com>
2025-04-18 17:31:40 +0200

    Update RCDC_arduino.ino
    Dziala wyswietlacz

--- RCDC_arduino.ino
+++ RCDC_arduino.ino
@@ -1,28 +1,76 @@
+#include <Wire.h>
 
-const int BUTTON_PIN = 2
-const int LED_PIN_1 = 3
-const int LED_PIN_2 = 5
+#include <LiquidCrystal_I2C.h>
 
+//WYSWIETLACZ oparty na PCF8574
+
+
+
+
+const int BUTTON_PIN = 2;
+const int LED_PIN_1 = 3;
+const int LED_PIN_2 = 5;
+const int countDelay = 1000;
+int newbutton = 0;
+int oldbutton = 0;// Poprzedni stan przycisku
+int clickCount = 0;
+int counter = 0;
+int extra_time = 0;
+unsigned long previousTime = 0;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Adres I2C może się różnić (np. 0x3F)
 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
+  
+  lcd.init();       // Inicjalizacja LCD
+  lcd.backlight();  // Włącz podświetlenie
+
+  lcd.setCursor(0, 0);
+}
+
+void countWithDelay() {
+  if (millis() - previousTime >= countDelay) {
+    previousTime = millis();
+    counter++;
+    Serial.println(counter);
+  }
+  
 }
 
 
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
   int buttonState = digitalRead(BUTTON_PIN);
+  countWithDelay();
+  
   if (buttonState==1)
-    {
+    { 
+       counter = 0;
        analogWrite(LED_PIN_1,255);
+    	lcd.setCursor(0, 0);
+  		lcd.print("1: H");
        analogWrite(LED_PIN_2,255);
-       delay(10000);
+    	lcd.setCursor(0, 1);
+  		lcd.print("2: H");
+    
+  }
+  else{
+  if(counter==10){
        analogWrite(LED_PIN_1,0);
-       delay(20000)
-       analogWrite(LED_PIN_2,0);
+    	lcd.setCursor(0, 0);
+  		lcd.print("1: L");
     }
+  if(counter==30){
+    analogWrite(LED_PIN_2,0);
+    lcd.setCursor(0, 1);
+  	lcd.print("2: L");
+    }
+  }
     
 }
