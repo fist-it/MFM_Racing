2505558a5181ade16314f429dd1759a0aa888faf
fist_it <fist_it@protonmail.com>
2025-05-08 10:40:15 +0200

    Engine Controller

--- RemoteCar/RemoteCar.ino
+++ RemoteCar/RemoteCar.ino
@@ -1,5 +1,20 @@
+#include<rf24>
+
+// [0] -> IN1
+// [1] -> IN2
+// [2] -> IN3
+// [3] -> IN4
+//
+// IN1 [0] - kanal A PRZOD
+// IN2 [1] - kanal A TYL
+//
+// IN3 [2] - kanal B PRZOD
+// IN4 [3] - kanal B TYL
+int engineControllerPins[] = {2, 3, 4, 5};
+
 
 void setup() {
+
 }
 
 void loop() {
