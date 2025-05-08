fdbdd52ff91195574457057c9b566d2e46b0dbca
fist_it <fist_it@protonmail.com>
2025-05-08 14:32:39 +0200

    Joystick, RemoteCar: parsing packets from joystick

--- RemoteCar/RemoteCar.ino
+++ RemoteCar/RemoteCar.ino
@@ -1,21 +1,108 @@
-#include<rf24>
-
-// [0] -> IN1
-// [1] -> IN2
-// [2] -> IN3
-// [3] -> IN4
+// IN1 - kanal A PRZOD
+// IN2 - kanal A TYL
 //
-// IN1 [0] - kanal A PRZOD
-// IN2 [1] - kanal A TYL
-//
-// IN3 [2] - kanal B PRZOD
-// IN4 [3] - kanal B TYL
-int engineControllerPins[] = {2, 3, 4, 5};
+// IN3 - kanal B PRZOD
+// IN4 - kanal B TYL
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define MOTOR_A_PIN         2
+#define MOTOR_A_REVERSE_PIN 3
+#define MOTOR_B_PIN         4
+#define MOTOR_B_REVERSE_PIN 5
+
+#define SPEED_A_PIN 0
+#define SPEED_B_PIN 0
+
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "47477";
+
+int joystick_X, joystick_Y;
+bool reverse = false;
 
 void setup() {
+  pinMode(MOTOR_A_PIN        , OUTPUT);
+  pinMode(MOTOR_A_REVERSE_PIN, OUTPUT);
+  pinMode(MOTOR_B_PIN        , OUTPUT);
+  pinMode(MOTOR_B_REVERSE_PIN, OUTPUT);
+
+  // Sterowanie predkoscia
+  pinMode(SPEED_A_PIN, OUTPUT);
+  pinMode(SPEED_B_PIN, OUTPUT);
+  // OPCJONALNE
+
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+void parse_packet(char msg[32]){
+  sscanf(msg, "%d %d", &joystick_X, &joystick_Y);
+
+  joystick_X = map(joystick_X, 0, 1023, -255, 255);
+  joystick_Y = map(joystick_Y, 0, 1023, -255, 255);
+
+  if (joystick_X == -1){
+    joystick_X = 0;
+  }
+  if(joystick_Y == -5){
+    joystick_Y = 0;
+  }
+  reverse = false;
+
+  if(joystick_Y<0){
+    reverse = true;
+    joystick_Y = abs(joystick_Y);
+  }
+
+  Serial.println("parsed x:");
+  Serial.println(joystick_X);
+
+  Serial.println("parsed y:");
+  Serial.println(joystick_Y);
+
+  Serial.println("Reverse:");
+  Serial.println(reverse);
 
 }
 
 void loop() {
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    parse_packet(text);
+    delay(1000);
+  }
+  
+  if(!reverse && joystick_Y){
+    analogWrite(MOTOR_A_PIN,         HIGH)
+    analogWrite(MOTOR_B_PIN,         HIGH)
+    analogWrite(MOTOR_A_PIN_REVERSE, LOW)
+    analogWrite(MOTOR_B_PIN_REVERSE, LOW)
+  } else if (joystick_Y){
+    analogWrite(MOTOR_A_PIN,         LOW)
+    analogWrite(MOTOR_B_PIN,         LOW)
+    analogWrite(MOTOR_A_PIN_REVERSE, HIGH)
+    analogWrite(MOTOR_B_PIN_REVERSE, HIGH)
+  } else {
+    analogWrite(MOTOR_A_PIN,         LOW)
+    analogWrite(MOTOR_B_PIN,         LOW)
+    analogWrite(MOTOR_A_PIN_REVERSE, LOW)
+    analogWrite(MOTOR_B_PIN_REVERSE, LOW)
+  }
+
+  if(joystick_X < 0) {
+    // Logika w prawo
+  } else {
+    // Logika w lewo
+  }
+  analogWrite(SPEED_A_PIN, joystick_Y);
+  analogWrite(SPEED_B_PIN, joystick_Y);
+
 }
